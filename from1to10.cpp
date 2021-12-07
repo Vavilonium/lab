921eac236325b18ff29267a112e28c91b18c24bd
Vavilonium <89990312+Vavilonium@users.noreply.github.com>
2021-12-07 19:58:41 +0500

    Посчитай до 10!

---
+++ from1to10.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "Числа от 1 до 10" << '\n';
+	int digit = 0;
+	while (digit < 11)
+	{
+		cout << digit << '\n';
+		digit += 1;
+	}
+
+}
