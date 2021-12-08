010e3f2702334e626f02d44ad3622575adb1cbc4
Vavilonium <89990312+Vavilonium@users.noreply.github.com>
2021-12-08 18:41:43 +0500

    Выделение массива нужного размера

---
+++ EvenNumbers.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int n;
+    int c = 0;
+    cout << "Введите длину массива: ";
+    cin >> n;
+    int* a = new int[n]; 
+    for (unsigned int i = 0; i < n; i++) {
+        c += 2;
+        a[i] = c;
+    }
+    for (unsigned int i = 0; i < n; i++) {
+        cout << a[i] << '\n';
+    }
+}
+
+
