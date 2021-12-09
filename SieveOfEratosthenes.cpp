67e500a0682a947162c17febecc491ece6838db2
Vavilonium <89990312+Vavilonium@users.noreply.github.com>
2021-12-09 13:37:34 +0500

    Решето Эратосфена

---
+++ SieveOfEratosthenes.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int n;
+    cout << "Введите число до которого нужно искать простые числа: ";
+    cin >> n;
+    int *a = new int[n + 1];
+    for (int i = 0; i <= n; i++)
+        a[i] = i;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (a[i])
+            for (int j = i * i; j <= n; j += i)
+                a[j] = 0;
+    }
+    for (int i = 2; i < n; i++)
+    {
+        if (a[i])
+        {
+            cout << a[i] << ' ';
+        }
+    }
+}
+
